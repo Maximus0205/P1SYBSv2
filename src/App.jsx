25f7b1a83f7599693e67dae49cd27a4e9e01ce42
Maximus0205 <Maximus0205@users.noreply.github.com>
2026-08-03 02:09:52 +0200

    ft3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SagListePage from "./pages/SagListePage";
-import SagView from "./components/SagView";
-import SagDele from "./components/SagDele";
-import AfstandsForslag from "./components/AfstandsForslag";
-import SagKortKompakt from "./components/SagKortKompakt";
-import CsvImport from "./components/CsvImport";
-import KvitteringUpload from "./components/KvitteringUpload";
-import NyeSagForm from "./components/NyeSagForm";
+
+import AdminSide from "./pages/main/AdminSide.jsx";
+import KoerselSide from "./pages/main/KoerselSide.jsx";
+import LagerSide from "./pages/main/LagerSide.jsx";
+import MontorSide from "./pages/main/MontorSide.jsx";
+import SalgSide from "./pages/main/SalgSide.jsx";
 
 export default function App() {
   return (
     <BrowserRouter basename="/P1SYBSv2">
       <Routes>
-        <Route path="/" element={<SagListePage />} />
-        <Route path="/sag/:id" element={<SagView />} />
-        <Route path="/dele/:id" element={<SagDele />} />
-        <Route path="/forslag" element={<AfstandsForslag />} />
-        <Route path="/kort" element={<SagKortKompakt />} />
-        <Route path="/import" element={<CsvImport />} />
-        <Route path="/upload" element={<KvitteringUpload />} />
-        <Route path="/ny" element={<NyeSagForm />} />
+        {/* Forside */}
+        <Route path="/" element={<MontorSide />} />
+
+        {/* Dine andre sider */}
+        <Route path="/admin" element={<AdminSide />} />
+        <Route path="/koersel" element={<KoerselSide />} />
+        <Route path="/lager" element={<LagerSide />} />
+        <Route path="/salg" element={<SalgSide />} />
       </Routes>
     </BrowserRouter>
   );
