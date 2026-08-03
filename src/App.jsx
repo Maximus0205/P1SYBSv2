99228371d5e8727cd189a738cf1c63635fa131e0
Maximus0205 <m-s-a@live.dk>
2026-08-03 19:36:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { MontorVaelger, MontorRuteView } from "./pages/MontorSide.jsx";
 import AdminSide from "./pages/AdminSide.jsx";
 import KoerselSide from "./pages/KoerselSide.jsx";
 import LagerSide from "./pages/LagerSide.jsx";
-import MontorSide from "./pages/MontorSide.jsx";
 import SalgSide from "./pages/SalgSide.jsx";
-
 
 export default function App() {
   return (
     <BrowserRouter basename="/P1SYBSv2">
       <Routes>
-        {/* Forside */}
-        <Route path="/" element={<MontorSide />} />
+        {/* Forside – vælg den du vil bruge */}
+        <Route path="/" element={<MontorVaelger />} />
 
-        {/* Dine andre sider */}
+        {/* Andre sider */}
+        <Route path="/rute" element={<MontorRuteView />} />
         <Route path="/admin" element={<AdminSide />} />
         <Route path="/koersel" element={<KoerselSide />} />
         <Route path="/lager" element={<LagerSide />} />
