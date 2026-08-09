4055347c8da45a26bc8e80b4d237c7093ffda57d
Maximus0205 <m-s-a@live.dk>
2026-08-09 21:13:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { supabase } from "./lib/supabaseClient";
 import {
-  hentSager, gemSager as gemSagerSky,
+  hentSager, gemSag as gemSagerSky,
   hentBiler, gemBil as gemBilerSky,
   hentVaretyper, gemVaretype as gemVaretyperSky,
   hentVarekategorier, gemVarekategori as gemVarekategorierSky,
